898f78100f469ddab3b8640df4e1400da45ad0aa
Rakesh Mahajan <rakeshmahajan@gmail.com>
2024-01-04 19:06:27 +0530

    Changed Ctrlstmt.kt. Removed classed and added main function Updated Interfaceex.kt. Added variable to interface

--- app/src/main/java/com/example/firstapp/Ctrlstmt.kt
+++ app/src/main/java/com/example/firstapp/Ctrlstmt.kt
@@ -1,8 +1,13 @@
 package com.example.firstapp
 
-fun main(){
-    var x = 10
-    val y = 11
+fun main(args: Array<String>){
+    var x : Int
+    val y : Int
+    x = args.get(0).toInt()
+    x = args[0].toInt()
+    //for ( arg in args){
+    //    println("Arg : $arg")
+    //}
 
     if (x > 100) {
         println("X is greater than 100")
@@ -21,31 +26,3 @@ fun main(){
 }
 
 
-class Ctrlstmt {
-    var x = 10
-    var y : Int = 12
-
-
-    companion object{
-
-        @JvmStatic
-        fun main(args: Array<String>){
-            var x = 10
-
-            if (x > 100) {
-                println("X is greater than 100")
-            }else if ( x > 0){
-                println("x is greater than 0")
-            }
-
-            when{
-                x > 100 -> {
-                    println("Using when : X is greater than 100")
-                }
-                x > 0 -> {
-                    println("Using when : X is greatin than 0")
-                }
-            }
-        }
-    }
-}
--- app/src/main/java/com/example/firstapp/Interfaceex.kt
+++ app/src/main/java/com/example/firstapp/Interfaceex.kt
@@ -2,12 +2,17 @@ package com.example.firstapp
 
 // Define an interface
 interface Shape {
+    var name : String
     fun area(): Double
     fun perimeter(): Double
 }
 
 // Implement the interface in a class
 class Circle(val radius: Double) : Shape {
+    override var name: String = "Circle"
+        get() = field
+        set(value) {}
+
     override fun area(): Double {
         return Math.PI * radius * radius
     }
@@ -18,6 +23,9 @@ class Circle(val radius: Double) : Shape {
 }
 
 class Rectangle(val width: Double, val height: Double) : Shape {
+    override var name: String
+        get() = "Rectangle"
+        set(value) {}
     override fun area(): Double {
         return width * height
     }
@@ -31,8 +39,11 @@ fun main() {
     // Create instances of classes implementing the Shape interface
     val circle = Circle(5.0)
     val rectangle = Rectangle(4.0, 6.0)
+    circle.name = "My circle"
 
     // Use the interface methods
     println("Circle - Area: ${circle.area()}, Perimeter: ${circle.perimeter()}")
     println("Rectangle - Area: ${rectangle.area()}, Perimeter: ${rectangle.perimeter()}")
+
+    println("Circle name : ${circle.name}")
 }
